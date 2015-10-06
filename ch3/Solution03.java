24831d2134f6dc55e71a63f0e55eb6b64da34dd5
Yayun Huang <arainhyy@gmail.com>
2015-10-06 14:19:17 -0400

    Update the follow up of ch3/Solution03.java

--- ch3/Solution03.java
+++ ch3/Solution03.java
@@ -1,50 +1,70 @@
-package codingbootcamp;
+package cc150;
 
 import java.util.*;
 
 class SetOfStacks {
 	final static int stack_threshold = 5;
-	private List <Stack <Integer>> st = new ArrayList<Stack <Integer>> ();
-	
+	private List<Stack<Integer>> st = new ArrayList<Stack<Integer>>();
+
 	public void push(int value) {
-		System.out.println(st.size());
 		if (st.isEmpty() || st.get(st.size() - 1).size() >= stack_threshold) {
-			st.add(new Stack <Integer> ());
+			st.add(new Stack<Integer>());
 		}
 		st.get(st.size() - 1).add(value);
 	}
-	
-	public void pop() {
-		Stack <Integer> one;
-		int i = 1;
-		// need to find the 1st nonempty stack
-		//	because pop operation can make some stacks empty
-		do {
-			one = st.get(st.size() - i ++);
-		} while (one.size() == 0);
-		one.pop();
+
+	public int pop() {
+		Stack<Integer> one;
+		one = st.get(st.size() - 1); // get the last stack
+		if (one.size() == 1)
+			st.remove(one);
+		return one.pop();
 	}
-	
+
 	public int peek() {
-		Stack <Integer> one;
-		int i = 1;
-		do {
-			one = st.get(st.size() - i ++);
-		} while (one.size() == 0);
+		Stack<Integer> one;
+		one = st.get(st.size() - 1);
 		return one.peek();
+	}
+
+	// move those stack after inx, need to move the first element to its latter
+	// stack
+	public int popAt(int inx) {
+		Stack<Integer> one;
+		int now = st.size() - 1;
+		int tmp;
+		int post_firstone = 0; // first element of the latter stack
+		while (now != inx) {
+			one = st.get(now);
+			tmp = one.remove(0); // remove the first element of this stack
+			if (one.isEmpty()) st.remove(one);
+			if (now != st.size() - 1)
+				one.push(post_firstone); // push the first element of latter stack
+			post_firstone = tmp;
+			now--;
+		}
+		one = st.get(inx);
+		
+		tmp = one.pop();
+		if (one.isEmpty()) st.remove(one);
+		
+		one.push(post_firstone);
+		return tmp;
 	}
 }
 
-public class codingbootcamp {
+public class cc150 {
 	public static void main(String[] args) {
 		SetOfStacks stack = new SetOfStacks();
-		for (int i=0; i<11; i++) {
+		for (int i = 0; i < 11; i++) {
 			stack.push(i);
 		}
 		System.out.println(stack.peek());
-		stack.pop();
-		System.out.println(stack.peek());
-		for (int i=0; i<8; i++) stack.pop();
-		System.out.println(stack.peek());
+		stack.popAt(2);
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
 	}
 }
